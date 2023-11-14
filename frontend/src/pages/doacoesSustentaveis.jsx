6178b42e5c38f3079bc9034380862e53d24347bb
xdhliti <guilherme.jacule@hotmail.com>
2023-11-14 18:53:54 -0300

    paginas

---
+++ frontend/src/pages/doacoesSustentaveis.jsx
@@ -0,0 +1,22 @@
+// src/pages/DoacoesSustentaveisPage.js
+import React, { useState } from 'react';
+
+const DoacoesSustentaveisPage = () => {
+
+  return (
+    <div>
+      <header>
+        <h1>DOAÇÕES SUSTENTÁVEIS</h1>
+        <p>
+          Nessa aba, você pode fazer doações seguras para ajudar pequenos e médios produtores alimentícios. A incorporação de blockchain nesse contexto pode trazer uma série de benefícios, incluindo transparência, rastreabilidade e eficiência nas transações.
+        </p>
+      </header>
+
+      <section>
+        {/* ai a gent eimplementa com blockcahin */}
+      </section>
+    </div>
+  );
+}
+
+export default DoacoesSustentaveisPage;
